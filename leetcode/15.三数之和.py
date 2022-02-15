35979d93217d877bc1a3d09010dc9a2cbdb28671
ViTo Tse <vitotse88@gmail.com>
2022-02-16 00:51:14 +0800

    add 15、16;

--- leetcode/15.三数之和.py
+++ leetcode/15.三数之和.py
@@ -7,5 +7,34 @@
 # @lc code=start
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        answers = list()
+        length = len(nums)
+        if length < 3: return []
+
+        for first in range(length):
+
+            if first > 0 and nums[first] == nums[first - 1]:
+                continue
+            
+            third = length - 1
+
+            target = -nums[first]
+
+            for second in range(first + 1, length):
+                if second > first + 1 and nums[second]== nums[second - 1]:
+                    continue
+
+                while second < third and nums[second] + nums[third] > target:
+                    third -= 1
+
+                if second == third:
+                    break
+
+                if nums[second] + nums[third] == target:
+                    answers.append([nums[first],nums[second], nums[third]])
+
+        return answers
+
 # @lc code=end
 
--- leetcode/16.最接近的三数之和.py
+++ leetcode/16.最接近的三数之和.py
@@ -5,7 +5,34 @@
 #
 
 # @lc code=start
+import math
+
+
 class Solution:
     def threeSumClosest(self, nums: List[int], target: int) -> int:
+        nums.sort()
+        n = len(nums)
+        if n < 3: return 0
+
+        ans = nums[0] + nums[1] + nums[2]
+        for first in range(n-2):
+            second = first + 1
+            third = n - 1
+
+            while second < third:
+                sum = nums[first] + nums[second] + nums[third]
+
+                if abs(sum - target) < abs(ans - target):
+                    ans = sum
+
+                if sum > target:
+                    third -= 1
+                else:
+                    second += 1
+                
+
+        return ans
+
+
 # @lc code=end
 
